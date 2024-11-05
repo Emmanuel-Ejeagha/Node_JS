43180aa2c8554f428e2d2791b467f94b6ea7e2f2
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-11-05 22:30:21 +0100

    Add: sum

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const divide = (a, b) => a / b;
 const multiply = (a, b) => a * b;
 
 console.log(sum(3, 7));
+
+const add = function sun(num1, num2) {
+  return num1 + num2;
+};
+
+console.log(add(34, 687));
