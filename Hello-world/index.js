dd952f5a14a7fcc6e0c566e6fc2755c57f018995
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-11-06 12:50:07 +0100

    modified

--- Hello-world/index.js
+++ Hello-world/index.js
@@ -3,7 +3,7 @@ const subtract = (a, b) => a - b;
 const divide = (a, b) => a / b;
 const multiply = (a, b) => a * b;
 
-console.log(sum(3, 7));
+console.log(sum(5, 6));
 
 const add = function sun(num1, num2) {
   return num1 + num2;
