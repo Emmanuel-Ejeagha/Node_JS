0ce93a86095785ec2435734620876bf7a04b2330
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-11-06 13:53:29 +0100

    Add: mkdirSync and writeFileSync

--- node-pkg-manager/index.js
+++ node-pkg-manager/index.js
@@ -1,16 +1,33 @@
+//  // path module
+// const path = require("path");
+
+// console.log("Directory name:", path.dirname(__filename));
+// console.log("File name:", path.basename(__filename));
+// console.log("File extension:", path.extname(__filename));
+
+// const joinPath = path.join("/user", "documents", "node", "projects");
+// console.log("Joined path", joinPath);
+
+// // resolve
+// const resolvePath = path.resolve("user", "documents", "node", "projects");
+// console.log("Resolved path", resolvePath);
+
+// //Normalize
+// const normalizePath = path.normalize("/user/.documents/../node/projects");
+// console.log("NormalizedPath", normalizePath);
+
+// // FILE SYSTEM
+const fs = require("fs");
 const path = require("path");
 
-console.log("Directory name:", path.dirname(__filename));
-console.log("File name:", path.basename(__filename));
-console.log("File extension:", path.extname(__filename));
+const myFolder = path.join(__dirname, "data");
 
-const joinPath = path.join("/user", "documents", "node", "projects");
-console.log("Joined path", joinPath);
+if (!fs.existsSync(myFolder)) {
+  fs.mkdirSync(myFolder);
+  console.log("Data folder created");
+}
 
-// resolve
-const resolvePath = path.resolve("user", "documents", "node", "projects");
-console.log("Resolved path", resolvePath);
-
-//Normalize
-const normalizePath = path.normalize("/user/.documents/../node/projects");
-console.log("NormalizedPath", normalizePath);
+const filePath = path.join(myFolder, "example.txt");
+// syn way of creating the file
+fs.writeFileSync(filePath, "Hello world from NodeJS");
+console.log("File created successfully");
