755442d17805708c7377fd91fe7647e89f1c7854
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-11-14 20:54:35 +0100

    Add: Updated database

--- bookstore-api/database/db.js
+++ bookstore-api/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectToDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://nuel6700:nuel6700db@cluster0.0pedh.mongodb.net/"
+      "mongodb+srv://nuel6700:nuel6700db@cluster0.gvuxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("Database is connected succesfully!");
   } catch (error) {
